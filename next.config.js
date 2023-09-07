5ed12906a46e8eccca7ac6377149acf5897a25d4
Jaynarol <jaynarol@gmail.com>
2023-09-07 12:06:54 +0700

    improve styling

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'frontendmentor-3-column-preview-card-component'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
+const REPO = 'frontendmentor-3-column-preview-card-component'
 const nextConfig = {
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  output: 'export'
 }
+
+if(process.env.NODE_ENV !== "development"){
+  const assetPrefix = `/${REPO}/`
+  const basePath = `/${REPO}`
+  nextConfig.assetPrefix = assetPrefix
+  nextConfig.basePath = basePath
+}
+
 
 module.exports = nextConfig
--- src/components/CarNode.tsx
+++ src/components/CarNode.tsx
@@ -11,16 +11,23 @@ const CarNode = (props: Props) => {
   const { name, desc, color, iconPath, iconAlt } = car
 
   return (
-    <div className={`${color.bg} p-10`}>
-      <Image width={64} height={40} alt={iconAlt} src={iconPath} />
+    <div className={`${color.bg} flex flex-col p-10 sm:h-[500px]`}>
+      <Image
+        width={64}
+        height={40}
+        alt={iconAlt}
+        src={iconPath}
+        className="flex-none"
+      />
       <h1
-        className={`${fontBigSHoulders.className} text-neutral-gray my-9 text-5xl uppercase`}
+        className={`${fontBigSHoulders.className} my-9 text-5xl uppercase text-neutral-gray`}
       >
         {name}
       </h1>
       <p className="text-neutral-white">{desc}</p>
+      <div className="grow"></div>
       <button
-        className={`${color.textButton} bg-neutral-gray mt-6 rounded-l-full rounded-r-full px-10 py-4`}
+        className={`${color.textButton} mt-6 w-fit rounded-l-full rounded-r-full bg-neutral-gray px-8 py-3`}
         type="button"
       >
         Learn More
