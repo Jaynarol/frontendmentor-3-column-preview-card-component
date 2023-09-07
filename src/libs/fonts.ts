ae52883486aa843d57398ec79c03ef6337b4c792
Jaynarol <jaynarol@gmail.com>
2023-09-07 09:55:05 +0700

    add style followed style-guide

---
+++ src/libs/fonts.ts
@@ -0,0 +1,11 @@
+import { Big_Shoulders_Display, Lexend_Deca } from 'next/font/google'
+
+export const fontBigSHoulders = Big_Shoulders_Display({
+  weight: '700',
+  subsets: ['latin'],
+})
+
+export const fontLexend = Lexend_Deca({
+  weight: '400',
+  subsets: ['latin'],
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'primary-range':  'hsl(31, 77%, 52%)',
+        'primary-cyan':  'hsl(184, 100%, 22%)',
+        'primary-dark':  'hsl(179, 100%, 13%)',
+        'neutral-white': 'hsla(0, 0%, 100%, 0.75)',
+        'neutral-gray': 'hsl(0, 0%, 95%)',
+      }
     },
   },
   plugins: [],
