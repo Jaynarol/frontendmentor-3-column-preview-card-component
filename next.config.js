76af98920e75a71e8dd4655fcf66c611e99522aa
Jaynarol <jaynarol@gmail.com>
2023-09-07 11:44:40 +0700

    config for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'frontendmentor-3-column-preview-card-component'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
